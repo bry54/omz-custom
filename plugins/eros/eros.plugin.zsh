4db385122620fd7f3fc9c4b5a7ef5ef62a9c93dd
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2024-07-28 16:49:46 +0300

    fixed plugin for containers to execute on the fly

--- plugins/eros/eros.plugin.zsh
+++ plugins/eros/eros.plugin.zsh
@@ -1,38 +1,60 @@
-bulkerosrname(){
-    # Directory containing files to be renamed
-    directory="$1"
-
-    # Check if the directory exists
-    if [ ! -d "$directory" ]; then
-        echo "Directory '$directory' not found."
-        return 1
-    fi
-
-    # Iterate over each file in the directory and its subdirectories
-    find "$directory" -type f -print0 | while IFS= read -r -d '' file; do
-        # Generate a random MD5 string
-        random_md5=$(md5string)
-
-        # Get the file extension
-        extension="${file##*.}"
-
-        # Rename the file with the random MD5 string and original extension
-        mv "$file" "${file%/*}/$random_md5.$extension"
-
-        # Print the old and new file names
-        echo "Renamed '$file' to '${file%/*}/$random_md5.$extension'"
-    done
-
-    echo "All files renamed successfully."
-}
-
 # Function to generate random MD5 string
 md5string() {
     #echo -n "$(date +%s%N)$RANDOM" | md5
     cat /dev/urandom | LC_CTYPE=C tr -dc 'a-f0-9' | head -c 32 | md5
 }
 
-starteros(){
+erosbulkrename(){
+    # Source directory containing files to be renamed
+    source_directory="$1"
+
+    # Destination directory to save renamed files
+    dest_directory="$2"
+
+    # Log file to store renaming details
+    log_file=".eroslog.txt"
+
+    # Check if the source directory exists
+    if [ ! -d "$source_directory" ]; then
+        echo "Source directory '$source_directory' not found."
+        return 1
+    fi
+
+    # Check if the destination directory exists, create if it does not
+    if [ ! -d "$dest_directory" ]; then
+        mkdir -p "$dest_directory"
+    fi
+
+    # Iterate over each file in the source directory and its subdirectories
+    find "$source_directory" -type f -print0 | while IFS= read -r -d '' file; do
+        # Get the file extension
+        extension="${file##*.}"
+
+        # Check if the file extension is 'eros'
+        if [ "$extension" = "eros" ]; then
+            # Generate a random MD5 string
+            random_md5=$(md5string)
+
+            # Extract the parent directory and the old file name
+            parent_dir=$(basename "$(dirname "$file")")
+            old_name=$(basename "$file")
+            new_name="$random_md5.$extension"
+
+            # New file path in the destination directory
+            new_file="$dest_directory/$new_name"
+
+            # Copy and rename the file to the destination directory
+            mv "$file" "$new_file"
+
+            # Print the old and new file names to the log file
+            echo "$new_name -> $parent_dir/$old_name" >> "$log_file"
+        fi
+    done
+
+    echo "All '.eros' files renamed and copied successfully. Log appended to '$log_file'."
+}
+
+erosserve(){
   EROS_HOME="$1"
   
   # Check if the directory exists
@@ -61,4 +83,66 @@ starteros(){
   -v "$EROS_HOME/blobs":/blobs \
   -v "$EROS_HOME/generated":/generated \
   stashapp/stash:latest
+}
+
+# Function to generate random MD5 string
+eroszip() {
+  zip -0 -r "$1" "$2"
+}
+
+eroscollect(){
+  source="$1"
+  destination="$2"
+  extenstions=("vid" "mp4")
+
+  # Check if the directory exists
+  if [ ! -d "$source" ]; then
+      echo "Source Directory '$source' not found."
+      return 1
+  fi
+
+  # Check if the directory exists
+  if [ ! -d "$destination" ]; then
+      echo "Destination Directory '$destination' not found."
+      return 1
+  fi
+
+  # Loop through each element in the array
+  for ext in "${extenstions[@]}"; do
+    echo "$source" "$ext" "eros"
+    bulkextensiontransformer "$source" "$ext" "eros"
+  done
+
+  erosbulkrename "${source}" "${destination}"
+
+  find "$base_directory" -type d -empty -exec rmdir {} +
+  echo "Empty folders cleaned up."
+}
+
+erosmockup(){
+  # Base directory where folders will be created
+  base_directory="$1"
+
+  # Create the base directory if it doesn't exist
+  mkdir -p "$base_directory"
+
+  # Loop to create 5 folders
+  for i in {1..5}; do
+      # Folder name
+      folder="$base_directory/folder$i"
+
+      # Create the folder
+      mkdir -p "$folder"
+
+      # Loop to create 3 files in each folder
+      for j in {1..3}; do
+          # File name
+          file="$folder/file$j.eros"
+
+          # Create the file
+          touch "$file"
+      done
+  done
+
+  echo "5 folders with 3 files each created successfully in $base_directory."
 }
--- plugins/fly-exec/fly-exec.plugin.zsh
+++ plugins/fly-exec/fly-exec.plugin.zsh
@@ -1,6 +1,7 @@
 function fly-exec-java(){
   APP_DIR="$1"
   WORKING_DIR="/usr/src/app"
+  
   docker run -it --rm \
   --name java-onthe-fly \
   --hostname java-onthe-fly \
@@ -13,6 +14,7 @@ function fly-exec-java(){
 function fly-exec-php(){
   APP_DIR="$1"
   WORKING_DIR="/var/www/html"
+  
   docker run -it --rm \
   --name php-onthe-fly \
   --hostname php-onthe-fly \
@@ -26,11 +28,23 @@ function fly-exec-php(){
 function fly-exec-node(){
   APP_DIR="$1"
   WORKING_DIR="/usr/src/app"
+  
   docker run -it --rm \
   --name node-onthe-fly \
   --hostname node-onthe-fly \
   -v "$APP_DIR":"$WORKING_DIR" \
   -w "$WORKING_DIR" \
   node:lts \
+  /bin/bash
+}
+
+function fly-exec-go(){
+  APP_DIR="$1"
+  WORKING_DIR="/usr/src/app"
+  
+  docker run -it --rm \
+  -v "$PWD":"$WORKING_DIR" \
+  -w "$WORKING_DIR" \
+  golang:1.22 
   /bin/bash
 }
